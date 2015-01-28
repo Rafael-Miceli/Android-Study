8a7cd7c02171958aafe6e165c0079df7729e8fb3
rafael-miceli <rafael.miceli@hotmail.com>
2015-01-28 16:22:17 -0200

    Implementing register page in custom auth app

---
+++ CustomLoginTest/app/src/main/java/com/example/rafaelmiceli/customlogintest/RegisterAccountActivity.java
@@ -0,0 +1,19 @@
+package com.example.rafaelmiceli.customlogintest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class RegisterAccountActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_account);
+
+
+    }
+
+}
