49f724267d6478edab8e058537194d9774d72fa2
rafael-miceli <rafael.miceli@hotmail.com>
2015-02-26 11:37:24 -0300

    Water down graph in SENA

--- MedicaoNivelAgua/app/src/main/java/pushtest/com/example/rafaelmiceli/pushtest/MyActivity.java
+++ MedicaoNivelAgua/app/src/main/java/pushtest/com/example/rafaelmiceli/pushtest/MyActivity.java
@@ -1,18 +1,102 @@
 package pushtest.com.example.rafaelmiceli.pushtest;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
-public class MyActivity extends Activity {
+import java.util.ArrayList;
+
+
+public class MyActivity extends FragmentActivity {
+
+    protected BarChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
+        mChart = (BarChart) findViewById(R.id.chart1);
+
+        mChart.setDrawValueAboveBar(true);
+
+        mChart.setDescription("");
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        mChart.setMaxVisibleValueCount(1);
+
+        // scaling can now only be done on x- and y-axis separately
+        mChart.setPinchZoom(false);
+
+        // draw shadows for each bar that show the maximum value
+        // mChart.setDrawBarShadow(true);
+
+        // mChart.setDrawXLabels(false);
+
+        mChart.setDrawGridBackground(false);
+        // mChart.setDrawYLabels(false);
+
+        // sets the text size of the values inside the chart
+        mChart.setValueTextSize(10f);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTypeface(tf);
+        xAxis.setDrawGridLines(false);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTypeface(tf);
+        leftAxis.setLabelCount(8);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setTypeface(tf);
+        rightAxis.setLabelCount(8);
+
+        mChart.setValueFormatter(new MyValueFormatter());
+
+        mChart.setValueTypeface(tf);
+
+        setData(1, 188);
+
+        Legend l = mChart.getLegend();
+        l.setEnabled(false);
+
+    }
+
+    private void setData(int count, float range) {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        xVals.add("Reservatório");
+
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+        yVals1.add(new BarEntry(range, 0));
+
+        BarDataSet set1 = new BarDataSet(yVals1, "DataSet");
+        set1.setBarSpacePercent(35f);
+
+        ArrayList<BarDataSet> dataSets = new ArrayList<BarDataSet>();
+        dataSets.add(set1);
+
+        BarData data = new BarData(xVals, dataSets);
+
+        mChart.setData(data);
     }
 
     @Override
